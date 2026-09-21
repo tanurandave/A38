dc80a4d10efe67dcd7f80d7f99638295437a9695
Tanuja Randave <randavetanuja@gmail.com>
2026-09-21 23:00:02 +0530

    pgm1

---
+++ Array/ContainerWater.java
@@ -0,0 +1,25 @@
+public class ContainerWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+            int width = right - left;
+
+            int minHeight = Math.min(height[left], height[right]);
+
+            int area = width * minHeight;
+
+            maxWater = Math.max(maxWater, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+} 
