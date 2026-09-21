a33cdedd64869423797b3edda17184063c1cec57
Tanuja Randave <randavetanuja@gmail.com>
2026-09-21 23:01:37 +0530

    pgm2

---
+++ Array/3Sum.java
@@ -0,0 +1,38 @@
+
+import java.util.Arrays;
+
+public class 3Sum {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // Update closest sum
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum;
+                }
+
+                // If sum equals target, it is the closest possible
+                if (sum == target) {
+                    return sum;
+                }
+
+                // Move pointers
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return closestSum;
+    }
+}
