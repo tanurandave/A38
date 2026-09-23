f411439f6ec68953fb59f5883c48a157d180ef62
Tanuja Randave <randavetanuja@gmail.com>
2026-09-23 23:02:33 +0530

    String pgm

---
+++ String/Anagram.java
@@ -0,0 +1,14 @@
+class Anagram {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26){
+           return false;
+        }
+
+        for(char ch = 'a'; ch<='z'; ch++){
+             if(sentence.indexOf(ch)==-1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
